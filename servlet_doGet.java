5e6dc68d7a5dd0f89f5cc6b56daf060c7a2dbccc
vedanthakur <vedanthakur@gmail.com>
2023-08-24 15:41:50 +0545

    Simple code

--- servlet_doGet.java
+++ servlet_doGet.java
@@ -1,3 +1,5 @@
+// The program will get data from this url
+// http://localhost:8080/HelloForm?first_name=ZARA&last_name=ALI
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -6,9 +8,9 @@ public class HelloForm extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\"> \n";
+        String docType = "<html><head><title>Names</title></head><body>";
         try {
-            out.println(docType + "<p>First Name: " + request.getParameter("first_name") + "<br>Second Name: " + request.getParameter("last_name") + "</p>");
+            out.println(docType + "<p>First Name: " + request.getParameter("first_name") + "<br>Second Name: " + request.getParameter("last_name") + "</p></body></html>");
         } catch (Exception e) {
             e.printStackTrace();
         }
