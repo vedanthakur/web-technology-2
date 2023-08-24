5f13957c49d1717c2b4eaefac1f110fdfb690e64
vedanthakur <vedanthakur@gmail.com>
2023-08-24 17:01:34 +0545

    add simple doGet() servlet

--- servlet_doGet.java
+++ servlet_doGet.java
@@ -1,18 +1,30 @@
 // The program will get data from this url
-// http://localhost:8080/HelloForm?first_name=Gazal&last_name=Thakur
+// http://localhost:8080/HelloForm?name=Gazal
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloForm extends HttpServlet {
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        String docType = "<html><head><title>Names</title></head><body>";
-        try {
-            out.println(docType + "<p>First Name: " + request.getParameter("first_name") + "<br>Second Name: " + request.getParameter("last_name") + "</p></body></html>");
-        } catch (Exception e) {
-            e.printStackTrace();
+        PrintWriter pw = response.getWriter();
+
+        // Get the name parameter
+        String name = request.getParameter("name");
+
+        pw.println("<html>");
+        pw.println("<head>");
+        pw.println("<title>Hello Form</title>");
+        pw.println("</head>");
+        pw.println("<body>");
+        if (name != null) {
+            pw.println("<p>Hello, <b>" + name + "</b>!</p>");
         }
+        pw.println("<p>Please specify your name.</p>");
+        pw.println("</body>");
+        pw.println("</html>");
+
     }
 }
